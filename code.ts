bdd9a27e565000f85d5e4f387f666e1cea5b4b84
jinsoo <binary01@kakao.com>
2024-03-11 20:45:59 +0900

    feat: binary figma Test

--- code.ts
+++ code.ts
@@ -7,15 +7,45 @@
 // full browser environment (See https://www.figma.com/plugin-docs/how-plugins-run).
 
 // This provides the callback to generate the code.
-figma.codegen.on('generate', (event) => {
+figma.codegen.on("generate", async (event) => {
   const code = `{
     name: "${event.node.name}"
   }`;
+  if (event.node.type === "INSTANCE") {
+    const css = await event.node.getCSSAsync();
+    const bg = css?.background;
+
+    const match = bg.match(/var\(--(.*?),/);
+
+    const variableName = match ? match[1] : null;
+
+    const cssCode = `{
+  bg-tailwind: "bg-${variableName}"
+}`;
+
+    // return [
+    //   {
+    //     language: "CSS",
+    //     code: cssCode,
+    //     title: "binary figma Test CSS",
+    //   },
+    // ];
+
+    const htmlCode = `<div className=bg-${variableName}>content</div>`;
+    return [
+      {
+        language: "HTML",
+        code: htmlCode,
+        title: "binary figma Test CSS",
+      },
+    ];
+  }
+
   return [
     {
       language: "PLAINTEXT",
       code: code,
-      title: "Codegen Plugin",
+      title: "binary figma Test",
     },
   ];
 });
